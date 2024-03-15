0f33c45b3d4375e0725524524b9d29d8a3d0bd23
Stefano Marello <smarello89@gmail.com>
2024-03-15 17:37:55 +0100

    Make `mix run` command to run Example.start by default

--- lib/example.ex
+++ lib/example.ex
@@ -1,18 +1,12 @@
 defmodule Example do
-  @moduledoc """
-  Documentation for `Example`.
-  """
+  use Application
+  def start(_type, _args) do
+    IO.puts(hello())
+    Supervisor.start_link([], strategy: :one_for_one)
+  end
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Example.hello()
-      :world
-
-  """
   def hello do
     :world
   end
+
 end
