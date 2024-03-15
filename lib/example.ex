19769e7e808c4e6e522a74dd13062fa37108c854
Stefano Marello <smarello89@gmail.com>
2024-03-15 17:42:17 +0100

    Add UUID dependency

--- lib/example.ex
+++ lib/example.ex
@@ -1,7 +1,10 @@
 defmodule Example do
   use Application
+  alias UUID
+
   def start(_type, _args) do
     IO.puts(hello())
+    IO.puts(UUID.uuid4())
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
