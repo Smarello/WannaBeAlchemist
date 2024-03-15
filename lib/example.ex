5cb54c2700cefd0339edde8a580402122d0628b4
Stefano Marello <smarello89@gmail.com>
2024-03-15 18:30:33 +0100

    Add strings and number exaple

--- lib/example.ex
+++ lib/example.ex
@@ -6,7 +6,9 @@ defmodule Example do
 
   def start(_type, _args) do
     # Example.main()
-    Example.caseExample()
+    # Example.caseExample()
+    # Example.stringsExample()
+    Example.numbersExample()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
@@ -30,6 +32,23 @@ defmodule Example do
       :bronze -> IO.puts("Get lost #{name}, you are not welcome here")
       _ -> IO.puts("Get lost #{name}, I don't even know you")
     end
+
+  end
+
+  def stringsExample do
+    IO.puts("Hello\nwelcolme\nto\nthe\nclub\n")
+    IO.puts("Interpolation looks like this: \#{}")
+    IO.puts(?a) #numeric unicode value of character a
+  end
+
+  def numbersExample do
+    a = 10
+    b = 2.0
+    IO.puts("a + b = #{a + b}")
+    :io.format("~.20f\n", [a / 100])
+    IO.puts(Float.ceil(0.5))
+    IO.puts(Float.ceil(0.1, 1)) # 0.1 is 0.10000000000000000555, which is ceiled at 0.2
+    IO.puts(Integer.gcd(10, 15)) # prints the greatest common divisor of 10 and 15
 
   end
 
