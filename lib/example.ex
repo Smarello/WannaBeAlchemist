68c231811d2979c841c8cbcc9cc49fd16c3941f1
Stefano Marello <smarello89@gmail.com>
2024-03-15 18:14:54 +0100

    Add case example

--- lib/example.ex
+++ lib/example.ex
@@ -1,15 +1,36 @@
 defmodule Example do
   use Application
+  @x 5 #define a constant
+
   alias UUID
 
   def start(_type, _args) do
-    IO.puts(hello())
-    IO.puts(UUID.uuid4())
+    # Example.main()
+    Example.caseExample()
     Supervisor.start_link([], strategy: :one_for_one)
   end
 
-  def hello do
-    :world
+  def main do
+    name = "John"
+    status = Enum.random([:gold, :silver, :bronze])
+    if (status === :gold) do
+      IO.puts("Hello #{name}, welcolme to the club")
+    else
+      IO.puts("Get lost #{name}, you are not welcome here")
+    end
+
+  end
+
+  def caseExample do
+    name = "John"
+    status = Enum.random([:gold, :silver, :bronze, :not_a_status])
+    case status do
+      :gold -> IO.puts("Hello #{name}, welcolme to the VIP club")
+      :silver -> IO.puts("Hello #{name}, you are a silver member, you can enter the club but you can't access the VIP area")
+      :bronze -> IO.puts("Get lost #{name}, you are not welcome here")
+      _ -> IO.puts("Get lost #{name}, I don't even know you")
+    end
+
   end
 
 end
